4919f9d815cba4651de0a1523475ed0ecfa9590f
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-25 09:17:39 +0300

    Added the shared memory class

--- mamas-storage/app.js
+++ mamas-storage/app.js
@@ -2,6 +2,8 @@ const inMemoryStorage = require("./index")
 
 var storage = new inMemoryStorage.InMemoryStorage()
 
+var storage2 = new inMemoryStorage.InMemoryStorage()
+
 storage.create("users", {username: "dar", password: "CorHorse4"})
 
 var f = (u) => u.username === "dar"
@@ -13,3 +15,49 @@ console.log(storage.remove("users", (u) => u.username === "dar"))
 storage.create("users", {username: "yael", password: "CorHorse4"})
 
 console.log(storage.where("users", {username:"yael"}))
+
+console.log("=====================================")
+
+storage2.create("users", {username: "dar", password: "CorHorse4"})
+
+var f = (u) => u.username === "dar"
+
+console.log(storage2.find("users", f))
+
+console.log(storage2.remove("users", (u) => u.username === "dar"))
+
+storage2.create("users", {username: "yael", password: "CorHorse4"})
+
+console.log(storage2.where("users", {username:"yael"}))
+
+console.log("=====================================")
+
+var sharedStorage = new inMemoryStorage.InMemorySharedStorage()
+
+sharedStorage.create("users", {username: "dar", password: "CorHorse4"})
+
+var f = (u) => u.username === "dar"
+
+console.log(sharedStorage.find("users", f))
+
+console.log(sharedStorage.remove("users", (u) => u.username === "dar"))
+
+sharedStorage.create("users", {username: "yael", password: "CorHorse4"})
+
+console.log(sharedStorage.where("users", {username:"yael"}))
+
+console.log("=====================================")
+
+var sharedStorage2 = new inMemoryStorage.InMemorySharedStorage()
+
+sharedStorage2.create("users", {username: "dar", password: "CorHorse4"})
+
+var f = (u) => u.username === "dar"
+
+console.log(sharedStorage2.find("users", f))
+
+console.log(sharedStorage2.remove("users", (u) => u.username === "dar"))
+
+sharedStorage2.create("users", {username: "yael", password: "CorHorse4"})
+
+console.log(sharedStorage2.where("users", {username:"yael"}))
--- mamas-storage/index.js
+++ mamas-storage/index.js
@@ -1,6 +1,8 @@
 const _ = require('lodash')
 const uuid = require('uuid')
 
+var sharedStorage = {}
+
 class InMemoryStorage{
     constructor(){
         this.storage = {}
@@ -50,19 +52,45 @@ class InMemoryStorage{
 
 class InMemorySharedStorage{
     create(collectionName, item){
-
+        var storedItem = item
+        storedItem._id = uuid.v4()
+        if(collectionName in sharedStorage){
+            sharedStorage[collectionName].push(storedItem)
+        }else{
+            sharedStorage[collectionName] = []
+            sharedStorage[collectionName].push(storedItem)
+        }
+        return storedItem
     }
 
     find(collectionName, findFunc){
-
+        var results = []
+        for(var i=0; i<sharedStorage[collectionName].length; i++){
+            results.push(_.find(sharedStorage[collectionName], (u) => findFunc(u)))
+        }
+        return results
     }
 
     where(collectionName, where){
-
+        var results = []
+        for(var i=0; i<sharedStorage[collectionName].length; i++){
+            if(_.isMatch(sharedStorage[collectionName][i], where)){
+                results.push(sharedStorage[collectionName][i])
+            }
+        }
+        return results
     }
 
     remove(collectionName, findFunc){
-        
+        var copyStorage = sharedStorage[collectionName]
+        var removed = []
+        for(var i=0; i<sharedStorage[collectionName].length; i++){
+            if(findFunc(sharedStorage[collectionName][i])){
+                removed.push(sharedStorage[collectionName][i])
+                copyStorage.splice(i, 1)
+            }
+        }
+        return removed
     }
 }
 
